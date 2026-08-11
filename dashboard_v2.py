c5b408f783d6c2b8807bd200bbe43fbbacc80a7c
unknown <chengyajing@xiaokacoffee.com>
2026-08-11 17:31:04 +0800

    Use relative path for data file

--- dashboard_v2.py
+++ dashboard_v2.py
@@ -25,9 +25,9 @@ st.set_page_config(
 # ==================== 数据文件路径配置 ====================
 # 在这里配置数据文件路径
 DATA_CONFIG = {
-    'sales_path': r'C:\商品销售数据\DATA\7月商品销售汇总表.csv',  
-    'bom_path': r'C:\商品销售数据\DATA\单杯物料消耗明细.xlsx',  
-    'order_path': r'C:\商品销售数据\DATA\7月报货单明细.xlsx', 
+    'sales_path': r'.\DATA\7月商品销售汇总表.csv',  
+    'bom_path': r'.\DATA\单杯物料消耗明细.xlsx',  
+    'order_path': r'.\DATA\7月报货单明细.xlsx', 
 }
 
 # ==================== 字体配置 ====================
